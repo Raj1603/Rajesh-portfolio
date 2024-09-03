d87ec7afd0c6fca749f85577748dbbd8c16cffd6
Raji1603 <rajiyamuna1603@gmail.com>
2024-09-03 13:37:42 +0530

    new files add

---
+++ index.js
@@ -0,0 +1,28 @@
+// let a=19;
+// if(a%2==0){
+//     console.log("The number is even")
+// } else{
+//     console.log("The number is odd")
+// }
+
+// let a=10;
+// let b=15;
+// let c=12;
+// if(a>b){console.log(a+"is the largest number")}
+// else if(b<c){
+//    console.log(c+"is the largest number")
+// }
+// else{console.log(b+"is the largest number")}
+
+//  a=18;
+//  b=20;
+//  c=29;
+//  if(a>b&&a>c){
+//     console.log(a+"is the largest number")
+//  }
+//  else if(b>c){
+//     console.log(b+"is the largest number")
+//  }
+//  else{
+//     console.log(c+"is the largest number")
+//  }
